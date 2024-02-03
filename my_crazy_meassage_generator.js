82a4b0c0fe21994fe7ba1a97cca82d986495155e
Markus Spiegler <83974913+Wolfsnight@users.noreply.github.com>
2024-02-03 02:29:14 +0100

    ADD ---> New file

---
+++ my_crazy_meassage_generator.js
@@ -0,0 +1,46 @@
+const name = [
+  "Anna",
+  "Ben",
+  "Celeste",
+  "Danni",
+  "Emma",
+  "Fred",
+  "Gerta",
+  "Hans",
+];
+
+const verb = [
+  "lebt",
+  "arbeitet",
+  "wandert",
+  "schläft",
+  "kuschelt",
+  "küsst",
+  "ißt",
+  "baded",
+];
+
+const pet = [
+  "Wolf",
+  "Löwe",
+  "Dinosauier",
+  "Pagagei",
+  "Elefant",
+  "Hai",
+  "Schlange",
+  "Kamel",
+];
+
+const place = [
+  "in die Stadt",
+  "auf den Mond",
+  "im Dschungel",
+  "auf die Alpen",
+  "auf einen Bauernhof",
+  "irgendwo im Nirgendwo",
+  "zwischen Himmel und Hölle",
+  "wo der Pfeffer wächst",
+];
+
+console.log()
+// "Anna" "lebt" mit "einen Wolf" "in die Stadt"
